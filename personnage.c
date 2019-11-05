43d49be2690d58c8e5774b4035eeffbfab9a07ad
yacine-bentoumia <56067669+yacine-bentoumia@users.noreply.github.com>
2019-11-05 17:51:49 +0000

    test fonction saut

--- personnage.c
+++ personnage.c
@@ -1,91 +1,145 @@
 #include "personnage.h"
 
+void personnage_jeu(SDL_Renderer *ecran, int h, int w, int positionY, SDL_Texture *perso, int numSprite, SDL_Rect *sprite, int affichage_position_x)
+{
 
-
-
-void personnage_jeu(SDL_Renderer* ecran ,int h, int w , int positionY,  SDL_Texture* perso, int numSprite,SDL_Rect* sprite,int affichage_position_x){
-
-    SDL_Rect  position ;
+    SDL_Rect position;
     //SDL_Rect* sprite = sprite_personnage();
 
-    
-    position.x = affichage_position_x*largeur_une_case(w);
-   
-    position.y = positionY * hauteur_une_case(h) ;
-    
+    position.x = affichage_position_x * largeur_une_case(w);
 
-    position.h = (h / HAUTEUR_MAP)*3 ;
-    position.w = w / LARGEUR_MAP ;
-    
-    SDL_RenderCopy(ecran, perso, &sprite[numSprite], &position) ;
-    
-      
+    position.y = positionY * hauteur_une_case(h);
+
+    position.h = (h / HAUTEUR_MAP) * 3;
+    position.w = w / LARGEUR_MAP;
+
+    SDL_RenderCopy(ecran, perso, &sprite[numSprite], &position);
 }
 
-SDL_Rect* sprite_personnage(){
+SDL_Rect *sprite_personnage()
+{
     SDL_Rect *sprite;
-    sprite = malloc(sizeof(SDL_Rect)*NB_CASE_PERSO);
+    sprite = malloc(sizeof(SDL_Rect) * NB_CASE_PERSO);
 
     int j = 0;
-    for (int i = 0 ; i < NB_CASE_PERSO ; i++){ 
+    for (int i = 0; i < NB_CASE_PERSO; i++)
+    {
         //hauteur d'une case
-        sprite[i].h = (HAUTEUR_SPRITE / NB_CASE_HAUTEUR) ; 
-    
+        sprite[i].h = (HAUTEUR_SPRITE / NB_CASE_HAUTEUR);
+
         //largeur d'une case
-        sprite[i].w = LARGEUR_SPRITE / NB_CASE_LARGEUR ;
+        sprite[i].w = LARGEUR_SPRITE / NB_CASE_LARGEUR;
 
         //coordonnees x du sprite a afficher
-        sprite[i].x = (i % NB_CASE_LARGEUR) * (sprite[i].w) ;
-        if ((i % NB_CASE_LARGEUR == 0) && (i != 0)){
-            
-            j++ ;
-        }
-        
-        //coordonnee y du sprite a afficher 
-        sprite[i].y = j * (sprite[i].h) ;
-          
-        
-    }
-    
-    return sprite ;
+        sprite[i].x = (i % NB_CASE_LARGEUR) * (sprite[i].w);
+        if ((i % NB_CASE_LARGEUR == 0) && (i != 0))
+        {
 
+            j++;
+        }
+
+        //coordonnee y du sprite a afficher
+        sprite[i].y = j * (sprite[i].h);
+    }
+
+    return sprite;
 }
 
-void deplacement_sur_map (int* debutX, int direction, int* positionX , int* positionY,int * affichage_position_x){
-    
-    if(((direction > 0) && (*positionX<LARGEUR_TABLEAU-1)) || (direction < 0 && *positionX > 0 )){
-        *positionX += direction ;
+void deplacement_sur_map(int *debutX, int direction, int *positionX, int *positionY, int *affichage_position_x)
+{
+
+    if (((direction > 0) && (*positionX < LARGEUR_TABLEAU - 1)) || (direction < 0 && *positionX > 0))
+    {
+        *positionX += direction;
     }
-    if(*debutX+(direction) <= LARGEUR_TABLEAU - LARGEUR_MAP && *debutX +(direction)>= 0){
-        *debutX = *debutX+direction ;
+    if (*debutX + (direction) <= LARGEUR_TABLEAU - LARGEUR_MAP && *debutX + (direction) >= 0)
+    {
+        *debutX = *debutX + direction;
+    }
+    else
+    {
 
-        
-    }else{
-
-       
-        if((*affichage_position_x < LARGEUR_MAP-1 && direction > 0) ||( direction < 0 && *affichage_position_x>0)){
-            *affichage_position_x +=direction;
-           
+        if ((*affichage_position_x < LARGEUR_MAP - 1 && direction > 0) || (direction < 0 && *affichage_position_x > 0))
+        {
+            *affichage_position_x += direction;
         }
-            
-    }   
-
-    
+    }
 }
 
-bool collision(int* map, int direction, int positionX, int positionY){
-   
-    //bool condition1 = direction == 1;
-    bool condition1 = map[(positionX + direction) + positionY * LARGEUR_TABLEAU] == 2 ; //position de x1 +1 == 2
-    bool condition2 = map[(positionX + direction) + (positionY + 1) * LARGEUR_TABLEAU] == 2 ; // position de x2 +1 ==2
-    bool condition3 = map[(positionX + direction) + (positionY + 2) * LARGEUR_TABLEAU] == 2 ;//position de x3 +1 == 2
-    return condition1 || condition2 || condition3 ;
+//fonction du saut du personnage
+void saut(int direction, int *positionX, int *positionY, int h)
+{
+
+    int temps_precedent = 0;
+    int temps_actuel = 0;
+
+    int position_maximale = *positionY + 4 * direction;
+    int ancienne_position = *positionY;
+
+    printf("position %d\n", *positionY);
+    printf("position a atteindre %d\n", position_maximale);
+
+    if ((*positionY < HAUTEUR_MAP - 1) && (*positionY >= 0))
+    {
+        while (*positionY > position_maximale)
+        {
+
+            temps_actuel = SDL_GetTicks();
+
+            // for (int i = 0; i < (5); i++)
+            // {
+
+            if (temps_actuel - temps_precedent > 50)
+            {
+
+                *positionY += direction;
+                printf("%d, %d\n", *positionY, temps_actuel);
+
+                //SDL_Delay(1000) ;
+                temps_precedent = temps_actuel;
+            }
+        }
+
+ 
+
+        // il redescend, pose problème
+
+        /* while (*positionY < ancienne_position)
+        {
+
+            temps_actuel = SDL_GetTicks();
+
+            // for (int i = 0; i < (5); i++)
+            // {
+
+            if (temps_actuel - temps_precedent > 50)
+            {
+
+                *positionY -= direction;
+                printf("%d, %d\n", *positionY, temps_actuel);
+
+                //SDL_Delay(1000) ;
+                temps_precedent = temps_actuel;
+            }
+            
+        }*/
+    }
+}
+
+
+bool collision(int *map, int direction, int positionX, int positionY)
+{
+
+    //ajouter la colision au niveau de la tete
+    bool condition1 = map[(positionX + direction) + positionY * LARGEUR_TABLEAU] == 2;       //position de x1 +1 == 2
+    bool condition2 = map[(positionX + direction) + (positionY + 1) * LARGEUR_TABLEAU] == 2; // position de x2 +1 ==2
+    bool condition3 = map[(positionX + direction) + (positionY + 2) * LARGEUR_TABLEAU] == 2; //position de x3 +1 == 2
+    return condition1 || condition2 || condition3;
 }
 
 // gerer lorsque le personnage atteint un trou
-bool trou(int* map, int positionX, int positionY){
-   
-    return map[positionX + (positionY + 3)* LARGEUR_TABLEAU] == 1 ;
+bool trou(int *map, int positionX, int positionY)
+{
+
+    return map[positionX + (positionY + 3) * LARGEUR_TABLEAU] == 1;
 }
-
-
