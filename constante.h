8dd7a13e98502c98f2b34cae765cc96fc37a5b89
yacine-bentoumia <56067669+yacine-bentoumia@users.noreply.github.com>
2019-11-21 21:18:21 +0000

    redéfinition de fichiers existants

--- constante.h
+++ constante.h
@@ -2,18 +2,31 @@
 #ifndef CONSTANTE_H
 #define CONSTANTE_H
 
-    #define LARGEUR_FENETRE 1300  //taille fenetre
-    #define HAUTEUR_FENETRE 900  // taille fenetre
-    #define SURFACE_MAP 2925  
-    #define HAUTEUR_MAP 45  // hauteur de lamap affichee dans la fenetre
-    #define LARGEUR_MAP 25 // largeur de la map affichee dans la fenetre 
-    #define LARGEUR_TABLEAU 65 //largeur de la carte du jeu
-    #define HAUTEUR_TABLEAU 45  // hauteur de la carte de jeu
-    #define LARGEUR_SPRITE 284 // sprite du personnage
-    #define HAUTEUR_SPRITE 256 // sprite du personnage
-    #define NB_CASE_LARGEUR 6  // personnage
-    #define NB_CASE_HAUTEUR 4  // personnage
-    #define NB_CASE_PERSO 24  //personnage
+    #define LARGEUR_FENETRE             1300  //taille fenetre
+    #define HAUTEUR_FENETRE             900  // taille fenetre
+
+    #define SURFACE_MAP                 2925
+    #define HAUTEUR_MAP                 45  // hauteur de lamap affichee dans la fenetre
+    #define LARGEUR_MAP                 25 // largeur de la map affichee dans la fenetre 
+
+    #define LARGEUR_TABLEAU             65 //largeur de la carte du jeu
+    #define HAUTEUR_TABLEAU             45  // hauteur de la carte de jeu
+
+    #define LARGEUR_SPRITE              284 // sprite du personnage
+    #define HAUTEUR_SPRITE              256 // sprite du personnage
+
+    #define LARGEUR_SPRITE_TOURELLE     608
+    #define HAUTEUR_SPRITE_TOURELLE     339
+
+    #define NB_CASE_LARGEUR             6  // personnage
+    #define NB_CASE_HAUTEUR             4  // personnage
+    #define NB_CASE_PERSO               24  //personnage
+
+    #define NB_SPRITE_STF               8
+    #define LARGEUR_STF                 32  //largeur du sprite de scientifique
+
+
+    #define NB_CASE_TOURELLE            1
 
     //#define LARGEUR_RACCOON 50
     //#define HAUTEUR_RACCOON 30
