f6021e431b27c7fe3848d595c5fb4534cc9565dc
yacine-bentoumia <56067669+yacine-bentoumia@users.noreply.github.com>
2019-11-29 01:38:03 +0000

    redefinition des structures

--- structures.c
+++ structures.c
@@ -1,54 +1,262 @@
 #include "structures.h"
 #include "constante.h"
 
-// structure commune à tous les personnages
-
-
-
 // Fonctions d'écriture
 
 // nouveau personnage
 
 // écriture
 
+// définition de base du ennemi
 
-
-// définition de base du raccoon
-
-raccoon definir_raccoon(SDL_Renderer *ecran, raccoon r)
+/*ennemi definir(SDL_Renderer *ecran, ennemi e)
 {
 
-    raccoon r;
+    ennemi e;
 
-    r.vie = 200;
+    e.vie = 200;
 
-    r.image = charger_image_transparente("rocket_image.bmp", ecran, 0, 137, 84);
+    e.image = charger_image_transparente("scientifique.bmp", ecran, 255, 0, 0);
 
-    r.sprite.x = 110 + 50;
-    r.sprite.y = 142;
-    r.sprite.w = LARGEUR_RACCOON;
-    r.sprite.h = HAUTEUR_RACCOON;
+    e.sprite.x = 110 + 50;
+    e.sprite.y = 142;
+    e.sprite.w = TAILLE_STF;
+    e.sprite.h = TAILLE_STF;
 
-    r.emplacement_sprite.x = 100;
-    r.emplacement_sprite.y = 600;
-    r.emplacement_sprite.w = 150;
-    r.emplacement_sprite.h = 100;
+    e.emplacement_sprite.x = 100;
+    e.emplacement_sprite.y = 600;
+    e.emplacement_sprite.w = 150;
+    e.emplacement_sprite.h = 100;
 
-    return r;
+    return e;
+}*/
+
+ennemi definir_nouveau_personnage(SDL_Renderer *ecran)
+{
+
+    ennemi e;
+
+    e.vie = 0;
+    e.vitesse = 0;
+    //e.nom = "";
+    e.image = NULL;
+    /*e.sprite = NULL;
+    e.emplacement_sprite = NULL;*/
+
+    return e;
 }
 
-// Fonctions lecture
+// fonctions d'écriture
 
-unsigned int lire_vie_raccoon(raccoon r)
+ennemi definir_vie(ennemi e, unsigned int vie)
 {
 
-    return r.vie;
+    e.vie = vie;
+
+    return e;
 }
 
-int lire_abscisse_raccoon(raccoon r)
+ennemi definir_vitesse(ennemi e, int vitesse)
 {
 
-    return r.emplacement_sprite.x;
+    e.vitesse = vitesse;
+
+    return e;
+}
+
+ennemi definir_nom(ennemi e, char *nom)
+{
+
+    for (int i = 0; i < 20; i++)
+    {
+
+        e.nom[i] = nom[i];
+    }
+
+    return e;
+}
+
+ennemi definir_image(ennemi e, SDL_Texture *image)
+{
+
+    e.image = image;
+
+    return e;
+}
+
+ennemi definir_sprite(ennemi e, SDL_Rect sprite)
+{
+
+    e.sprite = sprite;
+
+    return e;
+}
+
+ennemi definir_emplacement_sprite(ennemi e, SDL_Rect emplacement)
+{
+
+    e.emplacement_sprite = emplacement;
+
+    return e;
+}
+
+// Fonctions de lecture
+
+unsigned int lire_vie(ennemi e)
+{
+
+    return e.vie;
+}
+
+int lire_vitesse(ennemi e)
+{
+
+    return e.vitesse;
+}
+
+char *lire_nom(ennemi e)
+{
+
+    return e.nom;
+}
+
+/*void afficher_nom(ennemi e){
+
+    //printf("%s", lire_nom(e));
+
+    char* nom = lire_nom(e);
+
+    for (int i = 0; i < 20; i++){
+
+        printf("%c", nom[i]);         
+    }
+
+    printf("\n");
+}
+*/
+
+int lire_abscisse(ennemi e)
+{
+
+    return e.emplacement_sprite.x;
+}
+
+int lire_ordonnee(ennemi e)
+{
+
+    return e.emplacement_sprite.y;
+}
+
+void afficher_personnage(ennemi e, SDL_Renderer *ecran)
+{
+
+    SDL_RenderCopy(ecran, e.image, &e.sprite, &e.emplacement_sprite);
+}
+
+// SDL_Renderer *ecran;
+
+// définition du scientifique
+
+//ennemi scientist = definir_nouveau_personnage(ecran, scientist);
+/*unsigned int vie_scientist = 200;
+int vitesse_scientist = -10;          // négative car il se déplace vers le gauche
+const char* nom_scientist = "Scientifique";
+
+definir_vie(scientist, vie_scientist);
+definir_vitesse(scientist, vitesse_scientist);
+definir_nom(scientist, nom_scientist);*/
+
+
+
+
+/*
+
+A mettre dans ennemis.c
+
+*/
+
+
+/*SDL_Rect */ ennemi definir_sprite_stf(ennemi e){
+
+    e.sprite.x = 7*TAILLE_STF;
+    e.sprite.y = 0;
+    e.sprite.h = TAILLE_STF;
+    e.sprite.w = TAILLE_STF;
+
+    return e;
+
+}
+
+
+
+/*SDL_Rect*/ ennemi definir_emplacement_stf(ennemi e){
+
+    e.emplacement_sprite.x = 600;
+    e.emplacement_sprite.y = 620;
+    e.emplacement_sprite.w = 100;
+    e.emplacement_sprite.h = 80;
+
+    return e;
+
+}
+
+
+
+
+
+
+void animer_ennemi(ennemi e, SDL_Renderer *ecran)
+{
+
+    int temps_actuel = 0;
+    int temps_precedent = 0;
+
+    int parcours = -1; // sens du parcours de la feuille de sprite (-1: vers la gauche)
+
+    bool stop = false;
+
+    temps_actuel = SDL_GetTicks();
+    while (temps_actuel - temps_precedent > 100 && stop == false)
+    {
+
+        afficher_personnage(e, ecran);
+     
+        //printf("%dx, %dy\n", e.sprite.x, e.sprite.y);
+        e.emplacement_sprite.x -= 10;
+        e.sprite.x += parcours * (TAILLE_STF);
+
+        //printf("%dx, %dy\n", e.emplacement_sprite.x, e.emplacement_sprite.y);
+
+        if (e.sprite.x == 0 && e.sprite.y == 0)
+        {
+
+            //e.sprite.x = 7 * TAILLE_STF;
+            //e.sprite.x = 121;
+            //e.sprite.y = 59;
+            parcours = 1;
+            e.sprite.y += TAILLE_STF;
+            //moins = 1;
+        }
+
+        if (e.sprite.x == 8 * TAILLE_STF && e.sprite.y == TAILLE_STF)
+        {
+
+            e.sprite.x -= TAILLE_STF;
+            e.sprite.y += TAILLE_STF;
+            parcours = -1;
+        }
+
+        if (e.emplacement_sprite.x == -TAILLE_STF || (e.sprite.x == TAILLE_STF && e.sprite.y == 2 * TAILLE_STF))
+        {
+
+            stop = true;
+            SDL_DestroyTexture(e.image);
+        }
+
+        temps_precedent = temps_actuel;
+    }
+
+    
 }
 
 //lire emplacement...
@@ -59,11 +267,11 @@ typedef struct Personnage chat2;
 */
 
 /*
-void afficher_raccoon(SDL_Renderer *ecran, SDL_Event evenements, raccoon r) /*, SDL_Rect sprite, SDL_Rect emplacement_raccoon)*/
+void afficher(SDL_Renderer *ecran, SDL_Event evenements, ennemi e) /*, SDL_Rect sprite, SDL_Rect emplacement)*/
 /*{
 
     
-    r.sprite = sprite_raccoon(evenements, r);
+    e.sprite = sprite(evenements, r);
 
     // sprites de base
 
@@ -72,15 +280,15 @@ void afficher_raccoon(SDL_Renderer *ecran, SDL_Event evenements, raccoon r) /*, 
     // animation
 
 
-    // avant : raccoon est ici une texture
+    // avant : ennemi est ici une texture
 
-    SDL_RenderCopy(ecran, r.image, &r.sprite, &r.emplacement_sprite);
+    SDL_RenderCopy(ecran, e.image, &e.sprite, &e.emplacement_sprite);
 }
 
 */
 
 /*
-SDL_Rect sprite_raccoon(SDL_Event evenements, raccoon r)
+SDL_Rect sprite(SDL_Event evenements, ennemi e)
 {
 
     //sprite = malloc(sizeof(SDL_Rect) * NB_CASE_PERSO);
@@ -101,7 +309,7 @@ SDL_Rect sprite_raccoon(SDL_Event evenements, raccoon r)
         {
 
         case SDLK_r:
-            r.sprite.x += LARGEUR_RACCOON;
+            e.sprite.x += TAILLE_STF;
             //50;
             break;
         default:
@@ -109,14 +317,14 @@ SDL_Rect sprite_raccoon(SDL_Event evenements, raccoon r)
         }
     }
 
-    return r.sprite;
+    return e.sprite;
 }
 */
-/*void afficher_balle(SDL_Renderer *ecran, SDL_Texture *balle, bool afficher) /*, SDL_Rect sprite, SDL_Rect emplacement_raccoon)*/
+/*void afficher_balle(SDL_Renderer *ecran, SDL_Texture *balle, bool afficher) /*, SDL_Rect sprite, SDL_Rect emplacement)*/
 //{
 
 /* SDL_Rect position_balle;
-    SDL_Rect image = sprite_raccoon();
+    SDL_Rect image = sprite();
 
     position_balle.x = 100;
     position_balle.y = 600;
@@ -164,8 +372,8 @@ SDL_Rect sprite_balle()
 
     sprite.x = 110;
     sprite.y = 142;
-    sprite.w = LARGEUR_RACCOON;
-    sprite.h = HAUTEUR_RACCOON;
+    sprite.w = TAILLE_STF;
+    sprite.h = TAILLE_STF;
 
     return sprite;
 }*/
--- structures.h
+++ structures.h
@@ -3,42 +3,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 
 
-// structure commune à tous les personnages
+// structure pouvant être commune à tous les personnages
+// ici elle définit seulement les ennemis
 
 struct Personnage
 {
 
     unsigned int vie;
+    int vitesse;
+    /*const */char nom[20];
     SDL_Texture *image;
-    SDL_Rect sprite /*[NB_SPRITES_RACCOON]*/;
-    SDL_Rect emplacement_sprite /*[NB_SPRITES_RACCOON]*/;
+    SDL_Rect sprite; //[NB_SPRITES_RACCOON];
+    SDL_Rect emplacement_sprite; //[NB_SPRITES_RACCOON];
+
 };
 
 
 
-// définition du type raccoon
+// définition du type ennemi
 
-typedef struct Personnage raccoon;
+//typedef struct Personnage raccoon;
+typedef struct Personnage ennemi;
+//typedef struct ennemi scientifique;
 
 
-// fonctions de lecture et d'écriture
+// fonctions d'écriture
 
-raccoon definir_raccoon(SDL_Renderer *ecran, raccoon r);
-unsigned int lire_vie_raccoon(raccoon r);
-int lire_abscisse_raccoon(raccoon r);
+ennemi definir_nouveau_personnage(SDL_Renderer *ecran/*, ennemi e*/);
+ennemi definir_vie(ennemi e, unsigned int vie);
+ennemi definir_vitesse(ennemi e, int vitesse);
+ennemi definir_nom(ennemi e, char* nom);
+ennemi definir_image(ennemi e, SDL_Texture *image);
+ennemi definir_sprite(ennemi e, SDL_Rect sprite);
+ennemi definir_emplacement_sprite(ennemi e, SDL_Rect emplacement);
+
+
+
+// fonctions de lecture
+
+unsigned int lire_vie(ennemi e);
+int lire_vitesse(ennemi e);
+char* lire_nom(ennemi e);
+void afficher_nom(ennemi e);
+int lire_abscisse(ennemi e);
+int lire_ordonnee(ennemi e);
+void afficher_personnage(ennemi e, SDL_Renderer *ecran);
+
+
+//lire_image...
+
+
 
 
 // fonctions d'affichage
 
-void afficher_raccoon(SDL_Renderer *ecran, SDL_Event evenements, raccoon r)  ;
-SDL_Rect sprite_raccoon(SDL_Event evenements, raccoon r);
+void afficher(SDL_Renderer *ecran, SDL_Event evenements, ennemi e);
+SDL_Rect sprite(SDL_Event evenements, ennemi e);
+void annimer_ennemi(ennemi e, SDL_Renderer *ecran);
+
+
+
+
 void afficher_balle(SDL_Renderer *ecran, SDL_Texture *balle, bool afficher);
 SDL_Rect sprite_balle();
+
+
+
+
+// definiton du scientifique
+
+ennemi definir_sprite_stf(ennemi e);
+ennemi definir_emplacement_stf(ennemi e);
+
 
 
 #endif
